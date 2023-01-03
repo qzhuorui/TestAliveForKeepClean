743a336672bbb0f0a609685d89fe0ada41836db5
ChoRyan Quan <choryanquan@163.com>
2023-01-03 14:18:26 +0800

    fix:makeService增加同步方法

--- app/src/main/java/com/choryanquan/testaliveforkeepclean/kslk/servic/MakeService.java
+++ app/src/main/java/com/choryanquan/testaliveforkeepclean/kslk/servic/MakeService.java
@@ -20,6 +20,7 @@ import android.widget.RemoteViews;
 
 import androidx.core.app.NotificationCompat;
 
+import com.choryanquan.testaliveforkeepclean.AccountUtils;
 import com.choryanquan.testaliveforkeepclean.R;
 import com.choryanquan.testaliveforkeepclean.systsr.ScRcver;
 
@@ -163,6 +164,7 @@ public final class MakeService extends Service {
         super.onCreate();
         Log.d("aliveTest", "onCreate: MakeService");
         createNotify();
+        AccountUtils.startAccountSync(this);
         try {
             Intent intent = new Intent();
             intent.setClassName(getPackageName(), KuangService1.class.getName());
